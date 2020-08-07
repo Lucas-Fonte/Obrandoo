b612d4ed3a16ec6f01060c80d69d119fd5589e74
Lucas Souza da Fonte <fontelucas@yahoo.com.br>
2020-08-07 00:50:51 -0300

    testing deploy

---
+++ client/web-build/precache-manifest.2cb829d762b0edc42a68dd8086a41abd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca9b6ed71d8454915395",
+    "url": "/static/js/app.f11e94a9.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "495022447fa42e450fb8",
+    "url": "/static/js/2.8782c16e.chunk.js"
+  },
+  {
+    "revision": "f328c398676e066fb4e76f0c028592b4",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "034df3aaab73e4ae948a363fb202c198",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e802d2644326fd9fa701f1d969a8a94a",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "a990a8c98c379b1c14de33443329d80a",
+    "url": "/static/js/2.8782c16e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71033553ddc0843407efe54c23890fe2",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "c941078418f3ad7bb94a9ca94bc8b3dc",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "615280e83775685e49117729b227d0fc",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "2d4d439f00db18f2fc0a6835c0b35c9b",
+    "url": "/manifest.json"
+  }
+];
